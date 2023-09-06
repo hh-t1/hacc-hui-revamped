35356df0776238f746d78d3b7e4933596dae1f8c
kairitanaka <km321@hawaii.edu>
2023-09-06 05:20:52 -1000

    Update AgePage.jsx

--- app/imports/ui/pages/participant/AgePage.jsx
+++ app/imports/ui/pages/participant/AgePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Col } from 'react-bootstrap';
 import { ROUTES } from '../../../startup/client/route-constants';
 
 /**
@@ -10,8 +10,8 @@ import { ROUTES } from '../../../startup/client/route-constants';
 class AgePage extends React.Component {
   render() {
     return (
-        <div style={{ backgroundColor: '#393B44' }}>
-          <div align={'center'} style={{ backgroundColor: '#24252B' }}>
+        <Col style={{ backgroundColor: '#393B44' }}>
+          <Col align={'center'} style={{ backgroundColor: '#24252B' }}>
             <Container inverted style={{ padding: '5rem 10rem 5rem 10rem' }} as={'h2'}>
               Before we move onto making your profile, we need to verify your age.
               <br/>
@@ -23,8 +23,8 @@ class AgePage extends React.Component {
               <Button as={NavLink} activeClassName="active" exact to={ROUTES.UNDERAGE_PARTICIPATION}
                       style={{ color: 'white', backgroundColor: '#393B44' }} content="No, I am not." />
             </Container>
-          </div>
-        </div>
+          </Col>
+        </Col>
     );
   }
 }
