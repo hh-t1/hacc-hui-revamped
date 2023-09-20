f0171fa1f75fe9e8b36e48d2e26d70c72b442368
Kevin Nahinu <kevinan@hawaii.edu>
2023-09-20 01:22:25 -1000

    Clean Up
    - HelpPage loads properly
    - Cleaned up to look passable
    - Passed testcafe
    - Possible add changes later

--- app/imports/ui/pages/HelpPage.jsx
+++ app/imports/ui/pages/HelpPage.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { Link } from 'react-router-dom';
