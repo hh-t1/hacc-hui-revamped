5b5f83f35e426e7afcdbfe55d43386c04d791304
kairitanaka <km321@hawaii.edu>
2023-10-04 04:52:38 -1000

    Update AgePage.jsx

--- app/imports/ui/pages/participant/AgePage.jsx
+++ app/imports/ui/pages/participant/AgePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Header, Button } from 'semantic-ui-react';
+import { Container, Button } from 'react-bootstrap';
 import { ROUTES } from '../../../startup/client/route-constants';
 
 /**
@@ -10,21 +10,21 @@ import { ROUTES } from '../../../startup/client/route-constants';
 class AgePage extends React.Component {
   render() {
     return (
-        <div style={{ backgroundColor: '#393B44' }}>
-          <div align={'center'} style={{ backgroundColor: '#24252B' }}>
-            <Header inverted style={{ padding: '5rem 10rem 5rem 10rem' }} as={'h2'}>
-              Before we move onto making your profile, we need to verify your age.
-              <br/>
-              Are you 18 or over?
-              <br/>
-              <Button as={NavLink} activeClassName="active" exact to={ROUTES.PARTICIPATION}
-                  style={{ color: 'white', backgroundColor: '#393B44' }} content="Yes, I am." />
-              <br/>
-              <Button as={NavLink} activeClassName="active" exact to={ROUTES.UNDERAGE_PARTICIPATION}
-                      style={{ color: 'white', backgroundColor: '#393B44' }} content="No, I am not." />
-            </Header>
-          </div>
+      <div style={{ backgroundColor: '#393B44' }}>
+        <div align={'center'} style={{ backgroundColor: '#24252B' }}>
+          <Container inverted style={{ color: 'white', padding: '5rem 10rem 5rem 10rem' }} as={'h2'}>
+            Before we move onto making your profile, we need to verify your age.
+            <br/>
+            Are you 18 or over?
+            <br/>
+            <Button as={NavLink} activeClassName="active" exact to={ROUTES.PARTICIPATION}
+                    style={{ color: 'white', backgroundColor: '#393B44' }}>Yes, I am.</Button>
+            <br/>
+            <Button as={NavLink} activeClassName="active" exact to={ROUTES.UNDERAGE_PARTICIPATION}
+                    style={{ color: 'white', backgroundColor: '#393B44' }}>No, I am not.</Button>
+          </Container>
         </div>
+      </div>
     );
   }
 }
