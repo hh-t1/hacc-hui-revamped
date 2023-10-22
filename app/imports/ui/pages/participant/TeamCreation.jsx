4df32ed771a1da9ed7a579c689deeb4cea605b5b
kairitanaka <km321@hawaii.edu>
2023-10-21 16:21:58 -1000

    Update TeamCreation.jsx

--- app/imports/ui/pages/participant/TeamCreation.jsx
+++ app/imports/ui/pages/participant/TeamCreation.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Grid, Segment, Header, Divider, Loader } from 'semantic-ui-react';
-import {
-  AutoForm,
-  ErrorsField,
-  SubmitField,
-  TextField,
-  LongTextField,
-} from 'uniforms-semantic';
+import { Container, Col, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
 import PropTypes from 'prop-types';
-import { _ } from 'lodash';
 import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
+import { useTracker } from 'meteor/react-meteor-data';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import MultiSelectField from '../../components/form-fields/MultiSelectField';
-import RadioField from '../../components/form-fields/RadioField';
 import { Teams } from '../../../api/team/TeamCollection';
 import { Challenges } from '../../../api/challenge/ChallengeCollection';
 import { Skills } from '../../../api/skill/SkillCollection';
@@ -57,7 +49,6 @@ class TeamCreation extends React.Component {
   // eslint-disable-next-line no-unused-vars
   submit(formData, formRef) {
 
-    // console.log('CreateTeam.submit', formData, this.props);
     const skillsArr = this.props.skills;
     const skillsObj = [];
 
@@ -73,12 +64,10 @@ class TeamCreation extends React.Component {
       name, description, challenges, skills, tools, image,
     } = formData;
     let { open } = formData;
-    // console.log(challenges, skills, tools, open);
     if (open === 'Open') {
       open = true;
     } else {
       open = false;
-      // console.log('FALSE');
     }
 
     for (let i = 0; i < skillsArr.length; i++) {
@@ -121,7 +110,6 @@ class TeamCreation extends React.Component {
       skills: skillsObj,
       tools: toolsObj,
     };
-    // console.log(collectionName, definitionData);
     defineMethod.call({
           collectionName,
           definitionData,
@@ -129,53 +117,55 @@ class TeamCreation extends React.Component {
         (error) => {
           if (error) {
             swal('Error', error.message, 'error');
-            // console.error(error.message);
           } else {
             swal('Success', 'Team created successfully', 'success');
             formRef.reset();
-            //   console.log('Success');
           }
         });
-    // console.log(docID);
   }
 
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   render() {
-    // console.log(Teams.dumpAll());
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+    return (this.props.ready) ? this.renderPage() :
+      <div className="spinner-border" role="status">
+        <span className="sr-only">Getting data</span>
+      </div>;
   }
 
   renderPage() {
     let fRef = null;
     const formSchema = new SimpleSchema2Bridge(schema);
-    const challengeArr = _.map(this.props.challenges, 'title');
-    const skillArr = _.map(this.props.skills, 'name');
-    const toolArr = _.map(this.props.tools, 'name');
+    const challengeArr = this.props.challenges.map('title');
+    const skillArr = this.props.skills.map('name');
+    const toolArr = this.props.tools.map('name');
 
     return (
-        <Grid container centered>
-          <Grid.Column>
-            <Divider hidden />
+        <Container centered>
+          <Row>
+            <hr />
             <AutoForm ref={ref => {
               fRef = ref;
             }} schema={formSchema} onSubmit={data => this.submit(data, fRef)}
                       style={{
                         paddingBottom: '40px',
                       }}>
-              <Segment style={{
+              <ul
+                style={{
                 borderRadius: '10px',
                 backgroundColor: '#E5F0FE',
               }} className={'createTeam'}>
-                <Grid columns={1} style={{ paddingTop: '20px' }}>
-                  <Grid.Column style={{ paddingLeft: '30px', paddingRight: '30px' }}>
-                    <Header as="h2" textAlign="center" inverted>Team Information</Header>
-                    <Grid className='doubleLine'>
+                <Row columns={1} style={{ paddingTop: '20px' }}>
+                  <Col style={{ paddingLeft: '30px', paddingRight: '30px' }}>
+                    <Container as="h2" textAlign="center" inverted>Team Information</Container>
+                    <Col className='doubleLine'>
                       <TextField name='name' />
-                      <RadioField
-                          name='open'
-                          inline
-                      />
-                    </Grid>
+                      <div className="form-check">
+                        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label className="form-check-label" htmlFor="flexCheckDefault">
+                          open
+                        </label>
+                      </div>
+                    </Col>
                     <TextField name='image' placeholder={'Team Image URL'} />
                     <LongTextField name='description' />
                     <MultiSelectField name='challenges' placeholder={'Challenges'}
@@ -186,8 +176,8 @@ class TeamCreation extends React.Component {
                                       allowedValues={toolArr} required />
                     <TextField name="github" />
                     <TextField name="devpostPage" />
-                  </Grid.Column>
-                </Grid>
+                  </Col>
+                </Row>
                 <div align='center'>
                   <SubmitField value='Submit'
                                style={{
@@ -196,10 +186,10 @@ class TeamCreation extends React.Component {
                                }} />
                 </div>
                 <ErrorsField />
-              </Segment>
+              </ul>
             </AutoForm>
-          </Grid.Column>
-        </Grid>
+          </Row>
+        </Container>
     );
   }
 }
@@ -213,7 +203,7 @@ TeamCreation.propTypes = {
 
 };
 
-export default withTracker(() => {
+export default useTracker(() => {
   const subscriptionChallenges = Challenges.subscribe();
   const subscriptionSkills = Skills.subscribe();
   const subscriptionTools = Tools.subscribe();
