fe76c1cde1f3e7a4fc7ce5b44f64fcbe00760ab5
Kevin Nahinu <kevinan@hawaii.edu>
2023-11-01 07:24:08 -1000

    Update ShowMinorPage.jsx

--- app/imports/ui/pages/administrator/ShowMinorPage.jsx
+++ app/imports/ui/pages/administrator/ShowMinorPage.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import withAllSubscriptions from '../../layouts/AllSubscriptionsHOC';
 import ManageMinorWidget from '../../components/administrator/ManageMinorWidget';
 
-class ShowMinorPage extends React.Component {
-  render() {
+function ShowMinorPage() {
     return <ManageMinorWidget />;
   }
-}
 
 export default withAllSubscriptions(ShowMinorPage);
