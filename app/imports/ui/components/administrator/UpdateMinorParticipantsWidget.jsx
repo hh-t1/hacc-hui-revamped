4b4336290731eb8322f415d664646afa7495301b
ntomchak <55006670+ntomchak@users.noreply.github.com>
2023-11-15 04:08:11 -1000

    cleaned up UpdateMinorParticipantsCompliant and UpdateMinorParticipantsWidget

--- app/imports/ui/components/administrator/UpdateMinorParticipantsWidget.jsx
+++ app/imports/ui/components/administrator/UpdateMinorParticipantsWidget.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Table, Button, FormCheck, Container, Row, Col } from 'react-bootstrap';
 import swal from 'sweetalert';
@@ -11,15 +11,19 @@ import { MinorParticipants } from '../../../api/user/MinorParticipantCollection'
 import { ROUTES } from '../../../startup/client/route-constants';
 import { databaseFileDateFormat } from '../../pages/administrator/DumpDatabase';
 
-function UpdateMinorParticipantsWidget({ MinorParticipantsID }) {
+const UpdateMinorParticipantsWidget = (MinorParticipantsID) => {
   const [redirectToReferer, setRedirectToReferer] = useState(false);
   const [selected, setSelected] = useState([]);
 
   const getMinorParticipants = () => {
     const MinorParticipantsList = [];
     MinorParticipantsID.forEach((ParticipantsID) => {
-      const MinorParticipant = Participants._collection.findOne({ _id: ParticipantsID });
-      const MinorP = MinorParticipants._collection.findOne({ participantID: ParticipantsID });
+      const MinorParticipant = Participants._collection.findOne({
+        _id: ParticipantsID,
+      });
+      const MinorP = MinorParticipants._collection.findOne({
+        participantID: ParticipantsID,
+      });
       const ParentName = `${MinorP.parentFirstName} ${MinorP.parentLastName} (${MinorP.parentEmail})`;
       MinorParticipant.ParentName = ParentName;
       MinorParticipantsList.push(MinorParticipant);
@@ -29,13 +33,16 @@ function UpdateMinorParticipantsWidget({ MinorParticipantsID }) {
 
   const download = () => {
     const minors = getMinorParticipants();
-    let csv = 'Minor Participant Name, Participant email, Parent/Guardian Name (Parent/Guardian email)\n';
+    let csv =
+      'Minor Participant Name, Participant email, Parent/Guardian Name (Parent/Guardian email)\n';
     minors.forEach((m) => {
       csv += `${m.firstName} ${m.lastName},${m.username},${m.ParentName}\n`;
     });
     const zip = new ZipZap();
     const dir = 'hacchui-minor-participants';
-    const fileName = `${dir}/${moment().format(databaseFileDateFormat)}-minor-participants.csv`;
+    const fileName = `${dir}/${moment().format(
+      databaseFileDateFormat,
+    )}-minor-participants.csv`;
     zip.file(fileName, csv);
     zip.saveAs(`${dir}.zip`);
   };
@@ -51,7 +58,6 @@ function UpdateMinorParticipantsWidget({ MinorParticipantsID }) {
       updateMethod.call({ collectionName, updateData }, (error) => {
         if (error) {
           Error = true;
-          console.error('Could not update Participant', error);
         }
       });
     });
@@ -117,14 +123,14 @@ function UpdateMinorParticipantsWidget({ MinorParticipantsID }) {
       >
         <Table striped bordered hover responsive="sm">
           <thead style={{ backgroundColor: 'gray', color: 'white' }}>
-          <tr>
-            <th>Minor Participant Name</th>
-            <th>Parent Name (Email)</th>
-            <th>Compliant</th>
-          </tr>
+            <tr>
+              <th>Minor Participant Name</th>
+              <th>Parent Name (Email)</th>
+              <th>Compliant</th>
+            </tr>
           </thead>
           <tbody style={{ backgroundColor: 'white' }}>
-          {renderMinorParticipants()}
+            {renderMinorParticipants()}
           </tbody>
         </Table>
         <Container>
@@ -143,7 +149,7 @@ function UpdateMinorParticipantsWidget({ MinorParticipantsID }) {
       </div>
     </div>
   );
-}
+};
 
 UpdateMinorParticipantsWidget.propTypes = {
   MinorParticipantsID: PropTypes.arrayOf(PropTypes.string),
