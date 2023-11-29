55cb30a3361a5fdaf76452235c8b2fa99e828385
Ruben <rjacobo@hawaii.edu>
2023-11-28 23:57:01 -1000

    convert suggestToolSkillPage.jsx

--- app/imports/ui/pages/participant/SuggestToolSkillPage.jsx
+++ app/imports/ui/pages/participant/SuggestToolSkillPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import withAllSubscriptions from '../../layouts/AllSubscriptionsHOC';
 import SuggestToolSkillWidget from '../../components/participant/SuggestToolSkillWidget';
 
