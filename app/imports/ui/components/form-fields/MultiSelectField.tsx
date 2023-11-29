77fe0dcffbc9816423968447149f4d14f32e37a6
kairitanaka <km321@hawaii.edu>
2023-11-28 21:43:22 -1000

    semantic to bootstrap

--- app/imports/ui/components/form-fields/MultiSelectField.tsx
+++ app/imports/ui/components/form-fields/MultiSelectField.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import classnames from 'classnames';
 import { connectField, filterDOMProps } from 'uniforms';
-import { Dropdown } from 'semantic-ui-react';
-import _ from 'lodash';
+import { Dropdown } from 'react-bootstrap';
 
 /* eslint react/prop-types: 0 */
 const renderDropdown = ({ allowedValues, disabled, placeholder, onChange, transform, value }) => {
-  // console.log('renderMultiSelect value=%o allowedValues=%o', value, allowedValues);
-  const options = _.map(allowedValues, (val, index) => ({
+  const options = allowedValues.map((val, index) => ({
     key: index,
     text: transform ? transform(val) : val,
     value: val,
